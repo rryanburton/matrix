3f3ecfebed061bd06257cdd363fc4f6a1a0f73fb
Ryan Burton <rryanburton@gmail.com>
2015-09-10 00:31:34 -0400

    evening update

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,5 +1,5 @@
 class ShapeException(Exception):
-    pass
+    print("sorry those don't work together")
 
 def shape(vector):
     return (len(vector), )
@@ -20,22 +20,26 @@ def shape(vector):
 #     final = [(x + y) for item in zip(x, y) ]
 
 def vector_add(x, y):
-    final = [(x[r] + y[r]) for r in range(len(x))]
+    # final = [(x[r] + y[r]) for r in range(len(x))]
+    # return final
+    final = [[x[r][s] + y[r][s]  for r in range(len(x[0]))] for s in range(len(x))]
     return final
-
 def vector_sub(x, y):
     final = [(x[r] - y[r]) for r in range(len(x))]
     return final
 
 def vector_sum(*something):
-
+    sum = []
+    # sum = [[something[row][col][othr] for col in range(len(something[0]))] for row in range(len(something)for othr in range(len(something)]
     print(list(zip(*something)))
+
 
 
 def dot():
     pass
 def vector_multiply():
-    pass
+    result = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*Y)] for X_row in X]
+
 def vector_mean():
     pass
 
